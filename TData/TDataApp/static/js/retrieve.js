7963623d50b2a57362ec32aa4889ef78dbf43402
shardul <shardullingwal94@gmail.com>
2019-04-30 21:00:12 +0530

    just modifying

--- TData/TDataApp/static/js/retrieve.js
+++ TData/TDataApp/static/js/retrieve.js
@@ -1,91 +1,3 @@
-// var dbList=[];
-// var dbName="";
-// var relList=[];
-
-// $(document).ready(function(){
-//   $('#showDbDiv').hide();
-//   GetDBList();
-// })
-
-// function EmptyAll(){
-//   // <!-- works on db.change() as well -->
-  
-// }
-
-// function GetDBList(){
-//   $.ajax({
-//     type:'POST',
-//     url:"get_db/",
-//     async: false,
-//     success: function(response){
-//       console.log(response, typeof(response));
-//       if(response=="" || response==null || response.length==0){
-//         alert("empty");
-//       //   swal("Looks like we have no db currently\nWould you like to create one?",{
-//       //     buttons: {
-//       //       cancel: "No, I like it here!!",
-//       //       catch: {
-//       //         text: "Obviously!!",
-//       //         value: "catch",
-//       //       },
-//       //     },
-//       //   }).then((value)=>{
-//       //     switch(value){
-//       //       case "catch": 
-//       //         document.location.href="../create/";
-//       //     }
-//       //   });
-//       }
-//       else{
-//         // dbList=JSON.parse(response);
-//         dbList=response;
-//         console.log(dbList, typeof(response));
-//         console.log(dbList, typeof(dbList));
-//         // PopulateDB();
-//       }
-//     },
-//     error:function(err){
-//       alert(err.status);
-//       // swal('Something went wrong', '', 'error');
-//     }
-//   });
-//   console.log("get db list here");
-// }
-
-// function PopulateDB(){
-//   var str = "";
-//   console.log(dbList, typeof(dbList));
-//   for(i=0; i<dbList.length; i++){
-//     str+='<option value="'+dbList[i]+'">'+dbList[i]+'</option>';
-//   }
-//   $('#select_db').append(str);
-// }
-
-// function GetRel(){
-//   console.log("This function is already done in create.html");
-// }
-
-// function poplulateRel(){
-//   var i=0;
-//   var st = '';
-//   for(i=1; i<=relList.length; i++){
-//     st+='<option value="' + str(i) + '">' + relList[i-1] + '</option>'
-//   }
-//   return st;
-// }
-
-// function AddRel(){
-//   var sel = '<br><label for="join_rel">Join:</label><select class="form-control join_rel"></select>'
-//   var getOptions = poplulateRel();
-//   var btn = '<br><input type="button" class="btn btn-default" onClick="AddRel()" value="Join" style="float: right">'
-//   sel+=getOptions+btn;
-//   console.log(sel);
-//   $('#joinRelDiv').append(sel);
-  
-// }
-
-
-
 var dbList = [];
 var relList = [];
 var attrList = [];
@@ -146,7 +58,6 @@ function GetRelList(selectedDB, id){
       UpdateRelList(id);
     },
     error: function(response){
-      // alert(response.status);
       console.log(response.status);
     },
   })
@@ -228,7 +139,6 @@ function AddRel(){
   inp.setAttribute('type', 'text');
   inp.setAttribute('id', 'attrList'+x);
 
-  // SelRel.innerHTML = dropDown;
   document.getElementById('AllRelContainer').appendChild(SelRel);
   document.getElementById(x+1000).appendChild(dropDown);
   document.getElementById(x+1000).appendChild(attrForm);
@@ -246,10 +156,9 @@ function getAttrList(){
   for (i=1; i<=y; i++){
     var a_list = document.getElementById("attrList"+i).value;
     alert(a_list);
-    //This regex removes whitespaces, if any in attrlist.  
-    a_list = a_list.replace(/\s/g, "");      
+  
+    a_list = a_list.replace(/\s/g, ""); //This regex removes whitespaces, if any in attrlist.  
     
-    // Gives a list of attributes
     a_list = a_list.split(",");
     var e = document.getElementById(i)
     var chosen_rel = e.options[e.selectedIndex].text;
@@ -271,7 +180,6 @@ function getAttrList(){
 function Retrieve(){
   getAttrList();
   checkedRel = [];
-  // alert(compiled_list.length);
   $('#FromWrapper tr').each(function(){
     var isTemp = ($(this).find(".FromList:checked").val());
     var isTemp2 = ($(this).find(".relNameCheckBox").html());
@@ -282,14 +190,10 @@ function Retrieve(){
       checkedRel.push(isTemp2);
     }
   })
-  // console.log(checkedRel);
   
   var whereVal = $("#whereClause").val();
   var additionalQueryVal  = $("#additionalQuery").val();
 
-  // console.log(whereVal);
-  // console.log(additionalQueryVal);
-  
   data = {}
   data.dbName = selectedDB;
   data.relName = checkedRel;
@@ -303,7 +207,6 @@ function Retrieve(){
   $.ajax({
     type: "POST",
     url: "retrieveData/",
-    // contentType: "application/json",
     data: {
       'data': JSON.stringify(data)
     },
@@ -323,8 +226,6 @@ function Retrieve(){
       EmptyAll();
     }
   })
-
-
 }
 
 function FromCondition(){
@@ -334,62 +235,3 @@ function FromCondition(){
   }
   $("#FromWrapper").show();
 }
-
-// function Temporalize(){
-//   var tempo = 0;
-//   var table_data = []; //list of list containing isTemp and colName
-//   $('#addAttr tr').each(function() {
-//     var isTemp = ($(this).find(".temp:checked").val());
-//     console.log(isTemp, typeof(isTemp));
-//     if(isTemp=="on"){
-//       isTemp=1;
-//       tempo+=1;
-//     }
-//     else{
-//       isTemp=0;
-//     }
-    
-//     var attrName = ($(this).find(".colName").html())//.toString();
-//     var attrType = ($(this).find(".colType").html());
-//     if(attrName!="")
-//       table_data.push([isTemp, attrName, attrType]);
-//   })
-  
-//   console.log(table_data);
-//   if(tempo==0){
-//     swal('', 'No col selected for temporalizing', 'error');
-//     return;
-//   }
-  
-//   data = {};
-//   data.dbName = selectedDB;
-//   data.relName = selectedRel;
-//   data.attributes = table_data;
-//   console.log(data);
-  
-//   $.ajax({
-//     type: "POST",
-//     url: "temp_rel/",
-//     // contentType: "application/json",
-//     data: {
-//       'data': JSON.stringify(data)
-//     },
-//     async: false,
-//     // dataType:'text',
-//     success: function(response) {
-//       console.log(response, typeof(response));
-//       if (response == "1") {
-//         swal('Done', 'Table created.', 'success');
-//       } else if (response == "-1") {
-//         swal('Duplication detected for this relations', 'Contact admin for further queries', 'error');
-//       } else if(response == "0") {
-//         swal('Given attribute(s) already temporalized', 'Duplication detected for this attribute', 'success');
-//       } else {
-//         swal('Something went wrong', 'we will get back to you later', 'error');
-//       }
-//       EmptyAll();
-//     }
-//   })
-  
-// }
-
